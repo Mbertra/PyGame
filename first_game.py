9426d75174a1e0fa3f7db3c7f4215f1cd353b79a
mbertra <mbertra2@emich.edu>
2020-05-20 14:12:58 -0400

    Added welcome screen and a temporary background to help me get started

--- first_game.py
+++ first_game.py
@@ -1,7 +1,9 @@
 import pygame
+from pygame_functions import *
 
-# initialize pygame
+# initialize pygame and fonts
 pygame.init()
+font = pygame.font.Font('freesansbold.ttf', 32)
 
 # creates a window with a pixel width of 800x600
 window = pygame.display.set_mode((800, 600))
@@ -14,44 +16,70 @@ pygame.display.set_icon(icon)
 
 # Player
 player_img = pygame.image.load('transport.png')
-playerX = 370
-playerY = 480
-playerX_change = 0
+
+# The moving road will start with spacebar
+# Icons made by www.flaticon.com/authors/dinosoftlabs
+background_img = pygame.image.load('road2.png')
+background_img = pygame.transform.scale(background_img, (400, 800))
 
 
 def player(x, y):
     window.blit(player_img, (x, y))
 
 
-# run the window until it's told to exit
-running = True
-while running:
-    # sets the background of the game to black for now using R, G , B
-    window.fill((0, 0, 0))
+# displays an intro screen for the game
+def game_intro():
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            text_out = font.render('Press Space Bar to Start...', False, (96, 250, 255))
+            window.blit(text_out, (200, 300))
+            pygame.display.update()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    run_game()
+                    intro = False
+            if event.type == pygame.QUIT:
+                pygame.quit()
 
-        # keystroke event handler for left/right
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playerX_change = -5
-            if event.key == pygame.K_RIGHT:
-                playerX_change = 5
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                playerX_change = 0
 
-    # update player x
-    playerX += playerX_change
-    # boundaries
-    if playerX <= 200:
-        playerX = 200
-    if playerX >= 536:
-        playerX = 536
+def run_game():
+    playerX = 370
+    playerY = 480
+    playerX_change = 0
+    # run the window until it's told to exit
+    running = True
+    while running:
+        # sets the background of the game to black for now using R, G , B
+        window.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            # keystroke event handler for left/right
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    playerX_change = -5
+                if event.key == pygame.K_RIGHT:
+                    playerX_change = 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
+        window.blit(background_img, [200, 0])
 
-    # calls the player data
-    player(playerX, playerY)
-    # updates the game so changes are seen
-    pygame.display.update()
+        # update player x
+        playerX += playerX_change
+        # boundaries
+        if playerX <= 200:
+            playerX = 200
+        if playerX >= 536:
+            playerX = 536
+
+        # calls the player data
+        player(playerX, playerY)
+        # updates the game so changes are seen
+        pygame.display.update()
+
+
+# call needed methods
+game_intro()
