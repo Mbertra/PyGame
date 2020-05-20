1407d6980cf67136ee801de33c6eaf99caa6b8ad
mbertra <mbertra2@emich.edu>
2020-05-20 19:38:28 -0400

    Many changes including repeated car spawning, changing cordinates, and general syntax fixes including changing object variable to obstacle

--- first_game.py
+++ first_game.py
@@ -1,6 +1,7 @@
-import pygame
 import random
-import math
+import sched
+import time
+
 from pygame_functions import *
 
 
@@ -27,7 +28,7 @@ player_img = pygame.image.load('transport.png')
 
 # temporary objects image
 # TODO: update image add other objects
-object_image = player_img
+obstacle_image = player_img
 
 # Icons made by www.flaticon.com/authors/dinosoftlabs
 background_img = pygame.image.load('road2.png')
@@ -38,8 +39,8 @@ def player(x, y):
     window.blit(player_img, (x, y))
 
 
-def objects(x, y):
-    window.blit(object_image, (x, y))
+def obstacle(x, y):
+    window.blit(obstacle_image, (x, y))
 
 
 # displays an intro screen for the game
@@ -59,8 +60,8 @@ def game_intro():
 
 
 # find out if there is a collision
-def is_collision(objectX, objectY, playerX, playerY):
-    distance = math.sqrt((math.pow(objectX - playerX, 2)) + (math.pow(playerY - objectY, 2)))
+def is_collision(obstacleY, playerY, obstacleX, playerX):
+    distance = math.sqrt((math.pow(obstacleX - playerX, 2)) + (math.pow(playerY - obstacleY, 2)))
     if distance < 27:
         return True
     else:
@@ -68,20 +69,32 @@ def is_collision(objectX, objectY, playerX, playerY):
 
 
 def run_game():
+    running = True
     # variables for player
     playerX = 370
     playerY = 480
     playerX_change = 0
 
-    # variables for objects, TODO: make them spawn random, make them do damage
-    objectX = 225
-    objectY = 0
-    objectY_change = 0
+    # initialize obstacle
+    obstacleX = 225
+    obstacleY = 0
+
     # run the window until it's told to exit
-    running = True
+    counter = 0
     while running:
         # sets the background of the game to black for now using R, G , B
         window.fill((0, 0, 0))
+
+        # picks which spawn location for the car (at random from list of cords)
+        # current code can spawn cars on top of each other but never 4 cars
+        counter += 1
+        if counter % 100 == 0:
+            # variables for obstacles
+            spawn_cords = [225, 315, 415, 500]
+            num = spawn_cords[random.randint(0, 3)]
+            obstacleX = num
+            obstacleY = 0
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -94,31 +107,35 @@ def run_game():
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     playerX_change = 0
+        # displays the background image
         window.blit(background_img, [200, 0])
 
-        # this will spawn cars to avoid
-        objectY += 5
+        # calls the function to spawn cars and moves the y value of the cars
+        for i in range(0, 3):
+            obstacle(obstacleX, obstacleY)
+        # move the position of the car
+        obstacleY += 5
 
-        # update player x
+        # update players x coordinate
         playerX += playerX_change
-        # boundaries
+
+        # boundaries for player
         if playerX <= 200:
             playerX = 200
         if playerX >= 536:
             playerX = 536
 
+        # calls the player data
+        player(playerX, playerY)
+
+        # updates the game so changes are seen
+        pygame.display.update()
+
         # checks if there is a collision and if there is, restart from intro
-        collision = is_collision(objectX, objectY, playerX, playerY)
+        collision = is_collision(obstacleY, playerY, obstacleX, playerX)
         if collision:
             game_intro()
             break
-
-        # calls the player data
-        player(playerX, playerY)
-        # calls the objects
-        objects(objectX, objectY)
-        # updates the game so changes are seen
-        pygame.display.update()
 
 
 if __name__ == '__main__':
