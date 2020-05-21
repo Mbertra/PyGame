9f430ec1e70b901d634b275a4a37aed0ad7ad049
mbertra <mbertra2@emich.edu>
2020-05-21 14:37:50 -0400

    Many fixes, now spawns correct number of cars at correct intervals, need to fix so death works as intended as this release breaks it

--- first_game.py
+++ first_game.py
@@ -1,7 +1,6 @@
 import random
 import sched
 import time
-
 from pygame_functions import *
 
 
@@ -23,6 +22,10 @@ title = pygame.display.set_caption("Vroom Zoom")
 icon = pygame.image.load('car-2.png')
 pygame.display.set_icon(icon)
 
+# Icons made by www.flaticon.com/authors/dinosoftlabs
+background_img = pygame.image.load('road2.png')
+background_img = pygame.transform.scale(background_img, (325, 800))
+
 # Player
 player_img = pygame.image.load('transport.png')
 
@@ -30,17 +33,26 @@ player_img = pygame.image.load('transport.png')
 # TODO: update image add other objects
 obstacle_image = player_img
 
-# Icons made by www.flaticon.com/authors/dinosoftlabs
-background_img = pygame.image.load('road2.png')
-background_img = pygame.transform.scale(background_img, (400, 800))
+# Create an empty array
+obstacle_list = []
+
+# clock to keep track of some things
+clock = pygame.time.Clock()
 
 
 def player(x, y):
     window.blit(player_img, (x, y))
 
 
-def obstacle(x, y):
-    window.blit(obstacle_image, (x, y))
+def obstacle():
+    # Loop 3 times and add a snow flake in a random x,y position
+    for i in range(0, 3):
+        # variables for obstacles
+        spawn_cords = [215, 285, 370, 445]
+        num = random.randint(0, 3)
+        obstacleX = spawn_cords[num]
+        obstacleY = 0
+        obstacle_list.append([obstacleX, obstacleY])
 
 
 # displays an intro screen for the game
@@ -48,21 +60,22 @@ def game_intro():
     intro = True
     while intro:
         for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
             text_out = font.render('Press Space Bar to Start...', False, (96, 250, 255))
             window.blit(text_out, (200, 300))
             pygame.display.update()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    run_game()
                     intro = False
-            if event.type == pygame.QUIT:
-                pygame.quit()
+                    run_game()
 
 
 # find out if there is a collision
 def is_collision(obstacleY, playerY, obstacleX, playerX):
     distance = math.sqrt((math.pow(obstacleX - playerX, 2)) + (math.pow(playerY - obstacleY, 2)))
-    if distance < 27:
+    if distance < 50:
         return True
     else:
         return False
@@ -75,26 +88,13 @@ def run_game():
     playerY = 480
     playerX_change = 0
 
-    # initialize obstacle
-    obstacleX = 225
-    obstacleY = 0
-
     # run the window until it's told to exit
     counter = 0
     while running:
         # sets the background of the game to black for now using R, G , B
         window.fill((0, 0, 0))
 
-        # picks which spawn location for the car (at random from list of cords)
-        # current code can spawn cars on top of each other but never 4 cars
-        counter += 1
-        if counter % 100 == 0:
-            # variables for obstacles
-            spawn_cords = [225, 315, 415, 500]
-            num = spawn_cords[random.randint(0, 3)]
-            obstacleX = num
-            obstacleY = 0
-
+        # these statements check for key presses, and for user quits
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -107,14 +107,20 @@ def run_game():
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     playerX_change = 0
+
         # displays the background image
         window.blit(background_img, [200, 0])
 
+        # this is my way of setting a spawn speed
+        counter += 1
+        if counter % 75 == 0 or counter == 1:
+            obstacle()
+
         # calls the function to spawn cars and moves the y value of the cars
-        for i in range(0, 3):
-            obstacle(obstacleX, obstacleY)
-        # move the position of the car
-        obstacleY += 5
+        for k in range(len(obstacle_list)):
+            window.blit(obstacle_image, (obstacle_list[k][0], obstacle_list[k][1]))
+            # move the Y position of the car down n pixels
+            obstacle_list[k][1] += 3
 
         # update players x coordinate
         playerX += playerX_change
@@ -128,14 +134,20 @@ def run_game():
         # calls the player data
         player(playerX, playerY)
 
-        # updates the game so changes are seen
-        pygame.display.update()
-
         # checks if there is a collision and if there is, restart from intro
-        collision = is_collision(obstacleY, playerY, obstacleX, playerX)
-        if collision:
-            game_intro()
-            break
+        for k in range(len(obstacle_list)):
+            collision = is_collision(obstacle_list[k][1], playerY, obstacle_list[k][0], playerX)
+            if collision:
+                # ToDo : clear_board()
+                game_intro()
+                break
+        # updates the game so changes are seen
+        pygame.display.flip()
+
+        # limit fps to 60
+        clock.tick(60)
+    # closes everything
+    pygame.quit()
 
 
 if __name__ == '__main__':
